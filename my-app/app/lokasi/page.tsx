79cb63cc9806d934f5400dc1fefbaea93409b8a3
jovanakbarsetiodono-code <jovan_akbar_setiodono@teknokrat.ac.id>
2026-05-28 23:43:32 +0700

    Mengubah styling halaman lokasi menggunakan CSS Module

--- my-app/app/lokasi/page.tsx
+++ my-app/app/lokasi/page.tsx
@@ -1,46 +1,52 @@
+import styles from "./lokasi.module.css";
+
 export default function Lokasi() {
   return (
-    <div className="lokasi-page">
-      <div className="lokasi-container">
+    <div className={styles["lokasi-page"]}>
+      <div className={styles["lokasi-container"]}>
 
-        <h1 className="title">Lokasi Wira Garden</h1>
+        <h1 className={styles["lokasi-title"]}>
+          Lokasi Wira Garden
+        </h1>
 
-        <p className="description">
-            Wira Garden terletak di Batu Putu, Kecamatan Teluk Betung Utara, 
-            Kota Bandar Lampung, Provinsi Lampung.
-        </p>
+        <h4 className={styles["lokasi-description"]}>
+          Wira Garden terletak di Batu Putu, Kecamatan Teluk Betung Utara,
+          Kota Bandar Lampung, Provinsi Lampung.
+        </h4>
 
-        {/* TAMBAHAN WRAPPER */}
-        <div className="lokasi-wrapper">
+        <div className={styles["lokasi-wrapper"]}>
 
           <iframe
             src="https://www.google.com/maps?q=wira%20garden%20lampung&output=embed"
-            className="lokasi-map"
+            className={styles["lokasi-map"]}
           />
 
-        <div className="lokasi-info">
-            <h2 className="subtitle">Informasi Lokasi</h2>
+          <div className={styles["lokasi-info"]}>
 
-        <div className="lokasi-info-grid">
-            <p>📍 Batu Putu, Bandar Lampung</p>
-            <p>🕒 08.00 - 17.00 WIB</p>
-            <p>💰 Rp10.000 - Rp20.000</p>
-            <p>🚗 Motor & Mobil</p>
-        </div>
+            <h2 className={styles["subtitle"]}>
+              Informasi Lokasi
+            </h2>
+
+            <div className={styles["lokasi-info-grid"]}>
+              <p>📍 Batu Putu, Bandar Lampung</p>
+              <p>🕒 08.00 - 17.00 WIB</p>
+              <p>💰 Rp10.000 - Rp20.000</p>
+              <p>🚗 Motor & Mobil</p>
+            </div>
 
             <p>Fasilitas:</p>
-            <ul className="list">
+
+            <ul className={styles["list"]}>
               <li>Area parkir</li>
               <li>Tempat istirahat</li>
               <li>Spot foto</li>
               <li>Toilet umum</li>
             </ul>
 
-            <p>Rekomendasi: pagi atau sore hari</p>
+            <p>Rekomendasi: Pagi atau sore hari</p>
+
           </div>
-
         </div>
-
       </div>
     </div>
   );
