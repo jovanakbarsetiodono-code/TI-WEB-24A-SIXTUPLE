5e7b18ba5f7e3ab3141d0f77c902ab5b46f8cc7e
jovanakbarsetiodono-code <jovan_akbar_setiodono@teknokrat.ac.id>
2026-06-09 23:39:00 +0700

    menambahkan container HeroSection

--- my-app/components/herosection.tsx
+++ my-app/components/herosection.tsx
@@ -1,9 +1,11 @@
 export default function HeroSection() {
   return (
-    <section>
-      <h1>Wira Garden Lampung</h1>
+    <section className="hero-section">
+      <h1 className="home-title">
+        Wira Garden Lampung
+      </h1>
 
-      <h4>
+      <h4 className="home-subtitle">
         Nikmati keindahan alam dan suasana sejuk di Wira Garden Lampung
       </h4>
     </section>
