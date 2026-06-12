a9c0d54b28cde694f90539e32a78020f4dee10ea
arrasyaputra <ar'rasya_putra@teknokrat.ac.id>
2026-06-12 14:31:57 +0700

    Menghubungkan halaman Review dengan CSS Module

--- my-app/app/review/page.tsx
+++ my-app/app/review/page.tsx
@@ -1,12 +1,16 @@
+import styles from "./page.module.css";
 export default function ReviewPage() {
   return (
     <main>
-      <section>
-        <h1>Review Pengunjung</h1>
+        <section className="reviewHeader">
+  <h1>Review Wira Garden</h1>
+
+  <div className="line"></div>
         <p>
           Lihat pengalaman dan kesan para pengunjung yang telah menikmati
           keindahan Wira Garden.
         </p>
+
       </section>
     </main>
   );
