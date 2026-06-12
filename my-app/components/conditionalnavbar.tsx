b6763052a36b506877d7d301066a0f0656f2ca6f
jovanakbarsetiodono-code <jovan_akbar_setiodono@teknokrat.ac.id>
2026-06-12 19:41:04 +0700

    Menambahkan conditional navbar pada halaman admin

--- my-app/components/conditionalnavbar.tsx
+++ my-app/components/conditionalnavbar.tsx
@@ -6,7 +6,10 @@ import Navbar from "./navbar";
 export default function ConditionalNavbar() {
   const pathname = usePathname();
 
-  if (pathname.startsWith("/admin")) {
+  if (
+    pathname.startsWith("/admin") ||
+    pathname.startsWith("/laporan")
+  ) {
     return null;
   }
 
