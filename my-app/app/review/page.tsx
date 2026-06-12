121aeef1266ef2cad0d97c7763266e2ff647eda2
arrasyaputra <ar'rasya_putra@teknokrat.ac.id>
2026-06-12 13:59:07 +0700

    Membuat halaman Review Pengunjung

---
+++ my-app/app/review/page.tsx
@@ -0,0 +1,13 @@
+export default function ReviewPage() {
+  return (
+    <main>
+      <section>
+        <h1>Review Pengunjung</h1>
+        <p>
+          Lihat pengalaman dan kesan para pengunjung yang telah menikmati
+          keindahan Wira Garden.
+        </p>
+      </section>
+    </main>
+  );
+}
