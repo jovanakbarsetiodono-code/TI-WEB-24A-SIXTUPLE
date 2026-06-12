7ade372676e63aedec4320ffa5e6a83dbdd9f3aa
jovanakbarsetiodono-code <jovan_akbar_setiodono@teknokrat.ac.id>
2026-06-12 18:28:28 +0700

    Menyesuaikan navigasi pada halaman admin

---
+++ my-app/components/conditionalnavbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "./navbar";
+
+export default function ConditionalNavbar() {
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/admin")) {
+    return null;
+  }
+
+  return <Navbar />;
+}
