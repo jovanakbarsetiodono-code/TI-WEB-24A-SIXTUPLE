091dcba29e00e39c4018d059d174bd00f0774254
alungkhairunnas-max <alung_khairunnas@teknokrat.ac.id>
2026-06-13 00:25:43 +0700

    Menghapus beberapa keunggulan di halaman deskripsi

--- my-app/app/Deskripsi/page.tsx
+++ my-app/app/Deskripsi/page.tsx
@@ -46,9 +46,6 @@ export default function Page() {
           <li>Menyediakan banyak spot foto yang menarik.</li>
           <li>Menyediakan fasilitas yang memadai untuk pengunjung.</li>
           <li>Memiliki harga yang terjangkau.</li>
-          <li>
-            Menjadi salah satu destinasi wisata favorit di Bandar Lampung.
-          </li>
           <li>Keindahan alam yang masih terjaga dan tidak terlalu ramai.</li>
           <li>Cocok untuk kegiatan outdoor seperti gathering.</li>
           <li>Tempat yang aman untuk anak-anak bermain di area tertentu.</li> 
